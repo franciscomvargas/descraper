a71f81670c0c91c897fa9c8477aa3d895932f999
franciscomvargas <franciscomvargas24@gmail.com>
2023-07-14 06:23:42 +0100

    User Interface Start - Stone to be polished

--- server/serve.py
+++ server/serve.py
@@ -1,10 +1,23 @@
+import os
 from fastapi import FastAPI
 from .routehandlers import Handler
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 api = FastAPI(root_path="/api")
 
+root_file_path = os.path.dirname(os.path.abspath(__file__))
+static_folder_root = os.path.join(root_file_path, "ui/build")
+
 app.mount("/api", api)
+app.mount(
+    "/", 
+    StaticFiles(
+        directory=static_folder_root, 
+        html=True
+    ),
+    name="ui"
+)
 
 handlers = Handler()
 api.include_router(handlers.router)
