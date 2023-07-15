302542f05cbae90d961485e33822bc9404301c13
franciscomvargas <franciscomvargas24@gmail.com>
2023-07-15 16:11:45 +0100

    Add Request Header

--- scraper/webscraper.py
+++ scraper/webscraper.py
@@ -24,11 +24,32 @@ def html_scrape(url, refresh_html=False):
             }
         
     # Fetch Font Page html
-    fetch_retry_counter = 7
+    fetch_retry_counter = 4
+    # Request as Normal Computer
+    headers = {
+    "authority": "www.linkedin.com",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-language": "pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+        "cache-control": "max-age=0",
+        "cookie": "bcookie=^\^v=2",
+        "sec-ch-ua": "^\^Not.A/Brand^^;v=^\^8^^, ^\^Chromium^^;v=^\^114^^, ^\^Google",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "^\^Windows^^",
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    }
+
     while True:
-        scrape_res = httpx.get(url)
+        scrape_res = httpx.get(
+            url,
+            headers=headers
+        )
         print('Fetch Status:', scrape_res.status_code)
-        if scrape_res.status_code != 200:  # assuming the download was successful
+        if scrape_res.status_code != 200:  # Assert Request Sucess
             fetch_retry_counter -= 1
             if fetch_retry_counter == 0:
                 return {
