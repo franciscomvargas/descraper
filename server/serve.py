2433684de96b94d57a809cfafacd9893496d7b62
franciscomvargas <franciscomvargas24@gmail.com>
2023-07-15 15:34:56 +0100

    Added Coming Soon LandPage

--- server/serve.py
+++ server/serve.py
@@ -2,6 +2,7 @@ import os
 from fastapi import FastAPI
 from .routehandlers import Handler
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 api = FastAPI(root_path="/api")
@@ -9,7 +10,12 @@ api = FastAPI(root_path="/api")
 root_file_path = os.path.dirname(os.path.abspath(__file__))
 static_folder_root = os.path.join(root_file_path, "ui/build")
 
+@app.get('/comingsoon')
+async def index():
+    return FileResponse(os.path.join(root_file_path, "ui/build/comingsoon.html"))
+
 app.mount("/api", api)
+
 app.mount(
     "/", 
     StaticFiles(
