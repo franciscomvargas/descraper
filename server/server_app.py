e415f10522e1da4400f61049f525c10cf377a4a3
franciscomvargas <franciscomvargas24@gmail.com>
2023-08-21 04:07:18 +0100

    Deploy App W/Out reload on code edit

--- server/server_app.py
+++ server/server_app.py
@@ -1,7 +1,7 @@
 import uvicorn
 
 
-def launch_server(host="127.0.0.1", port=8880, workers=1, reload=True):
+def launch_server(host="127.0.0.1", port=8880, workers=1, reload=False):
     uvicorn.run("server.serve:app", host=host, port=port, workers=workers,
                 log_level="info", reload=reload)
 
