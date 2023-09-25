9459e72933ae482f027f752494e1f5b9c7ea1bd2
franciscomvargas <franciscomvargas24@gmail.com>
2023-09-26 00:09:54 +0100

    DeSOTA read url from file in model api request

--- runner_utils/utils.py
+++ runner_utils/utils.py
@@ -6,6 +6,58 @@ import re
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 API_UP =  "http://129.152.27.36/assistant/api_uploads"
 
+# QUESTION-ANSWER MODEL
+def parse_text(desota_arg):
+    # TEMPORARY FIX
+    # retrieved from https://stackoverflow.com/a/3642850 & https://stackoverflow.com/a/32680048
+    _file_pattern = re.compile(r'file\(((.)*)\)')
+    _file_res = _file_pattern.search(desota_arg)
+    if _file_res:
+        desota_arg = _file_res.group(1)
+    _text_pattern = re.compile(r'text\(((.)*)\)')
+    _text_res = _text_pattern.search(desota_arg)
+    if _text_res:
+        desota_arg = _text_res.group(1)
+
+
+    if desota_arg.endswith(".txt"):
+        download_desota_url = f'{API_UP}/{desota_arg}'
+        base_filename = os.path.basename(desota_arg)
+        file_path = os.path.join(CURRENT_PATH, base_filename)
+        with  requests.get(download_desota_url, stream=True) as req_file:
+            if req_file.status_code != 200:
+                return None
+            
+            if req_file.encoding is None:
+                req_file.encoding = 'utf-8'
+
+            with open(file_path, 'w') as fw:
+                fw.write("")
+            with open(file_path, 'a', encoding=req_file.encoding) as fa:
+                for line in req_file.iter_lines(decode_unicode=True):
+                    if line:
+                        fa.write(f"{line}\n")
+                        # shutil.copyfileobj(req_file.raw, fwb)
+
+        with open(file_path, 'r', encoding=req_file.encoding) as fr:
+            _text_res = "\n".join(fr.readlines())
+
+        os.remove(file_path)
+
+        return _text_res
+
+    else:
+        return desota_arg
+    
+def get_request_qa(model_request_dict):
+    _context, _question = None, None
+    if "context" in model_request_dict["input_args"]:
+        _context = parse_text(model_request_dict["input_args"]["context"])
+    if "question" in model_request_dict["input_args"]:
+        _question = parse_text(model_request_dict["input_args"]["question"])
+
+    return _context, _question
+
 
 # URL MODEL
 def get_url_from_str(string):
@@ -16,22 +68,45 @@ def get_url_from_str(string):
     url = re.findall(regex, string)
     return [x[0] for x in url]
 
+def get_url_from_file(file_name):
+    # TEMPORARY FIX
+    # retrieved from https://stackoverflow.com/a/3642850 & https://stackoverflow.com/a/32680048
+    _file_pattern = re.compile(r'file\(((.)*)\)')
+    _file_res = _file_pattern.search(file_name)
+    if _file_res:
+        file_name = _file_res.group(1)
+    _text_pattern = re.compile(r'text\(((.)*)\)')
+    _text_res = _text_pattern.search(file_name)
+    if _text_res:
+        file_name = _text_res.group(1)
+
+    file_url =f'{API_UP}/{file_name}'
+    
+    file_content = ""
+    with  requests.get(file_url, stream=True) as req_file:
+        if req_file.status_code != 200:
+            return None
+        
+        if req_file.encoding is None:
+            req_file.encoding = 'utf-8'
+
+        for line in req_file.iter_lines(decode_unicode=True):
+            if line:
+                file_content += line
+                
+    return get_url_from_str(file_content)
+
 def get_request_url(model_request_dict):
     _req_url = None
     if 'url' in model_request_dict["input_args"]:
         _req_url = get_url_from_str(model_request_dict["input_args"]['url'])
     
+    if not _req_url and 'file' in model_request_dict["input_args"] and "file_name" in model_request_dict["input_args"]["file"]:
+        _req_url = get_url_from_file(model_request_dict["input_args"]['file']['file_name'])
+
     if not _req_url and 'text_prompt' in model_request_dict["input_args"]:
         _req_url = get_url_from_str(model_request_dict["input_args"]['text_prompt'])
 
-    if not _req_url and 'file' in model_request_dict["input_args"]:
-        if isinstance(model_request_dict["input_args"]["file"], str):
-            _req_url = get_url_from_str(model_request_dict["input_args"]['file'])
-        elif isinstance(model_request_dict["input_args"]["file"], list):
-            for file in model_request_dict["input_args"]["file"]:
-                _req_url = get_url_from_str(file)
-                if _req_url:
-                    break
     return _req_url
 
 
