a5da19c246d975b91359324152a549166fb8f13e
franciscomvargas <franciscomvargas24@gmail.com>
2023-10-21 00:47:51 +0100

    Windows Service Status CMD log

--- executables/Windows/descraper.status.bat
+++ executables/Windows/descraper.status.bat
@@ -5,19 +5,16 @@ set service_name=descraper_service
 :: %~dp0 = C:\users\[user]\Desota\Desota_Models\DeScraper\executables\Windows
 for %%a in ("%~dp0..\..\..\..\..") do set "root_path=%%~fa"
 
-
 :: -- Edit bellow if you're felling lucky ;) -- https://youtu.be/5NV6Rdv1a3I
-:: Bat Requires as argument the Target where status will be written
-IF "%1" EQU "" (
-    GOTO exit_bat
-)
 
 :: NSSM - exe path 
 IF %PROCESSOR_ARCHITECTURE%==AMD64 set nssm_exe=%root_path%\Desota\Portables\nssm\win64\nssm.exe
 IF %PROCESSOR_ARCHITECTURE%==x86 set nssm_exe=%root_path%\Desota\Portables\nssm\win32\nssm.exe
 
 :: Status service - retrieved from https://nssm.cc/commands
+:: Bat Requires as argument the Target where status will be written
+IF "%1" EQU "" GOTO noargs
 %nssm_exe% status %service_name% >%1
-
-:exit_bat
 exit
+:noargs
+%nssm_exe% status %service_name%
