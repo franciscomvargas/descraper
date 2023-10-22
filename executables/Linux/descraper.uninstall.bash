3b751cd3b03c0d45b4ac063416815410c6bd56a1
Francisco Vargas <87917356+franciscomvargas@users.noreply.github.com>
2023-10-22 03:45:17 +0100

    Model Standardize for DeSOTA DEV

--- executables/Linux/descraper.uninstall.bash
+++ executables/Linux/descraper.uninstall.bash
@@ -4,11 +4,24 @@
 get_user=$(who)
 USER=${get_user%% *}
 USER_HOME="/home/$USER"
+
+
+
+# -- Edit bellow vvvv DeSOTA DEVELOPER EXAMPLe: miniconda + pip pckgs + systemctl service
+
 # Setup VARS
 MODEL_NAME=DeScraper
+# - Systemctl service
 MODEL_SERVICE=descraper.service
 # - Model Path
 MODEL_PATH=$USER_HOME/Desota/Desota_Models/$MODEL_NAME
+# - Conda Environment
+MODEL_ENV=$MODEL_PATH/env
+CONDA_PATH=$USER_HOME/Desota/Portables/miniconda3/bin/conda
+
+
+
+# -- Edit bellow if you're felling lucky ;) -- https://youtu.be/5NV6Rdv1a3I
 
 # SUPER USER RIGHTS
 [ "$UID" -eq 0 ] || { 
@@ -69,11 +82,21 @@ else
 fi
 
 # STOP SERVICE
+echo
+echo "Disabling Model Service..."
+echo "    Model Name: $MODEL_SERVICE"
 systemctl stop $MODEL_SERVICE
 systemctl disable $MODEL_SERVICE
+
 if [ "$quiet" -eq "0" ]; 
     then
+    # DELETE PCKGS
+    $CONDA_PATH remove --prefix $MODEL_ENV --all --force
+    
     # DELETE SERVICE
+    echo
+    echo "Deleting Model Service..."
+    echo "    Service path: /lib/systemd/system/$MODEL_SERVICE"
     rm -r /lib/systemd/system/$MODEL_SERVICE
     rm -r /etc/systemd/system/$MODEL_SERVICE&> /dev/null # and symlinks that might be related
     rm -r /usr/lib/systemd/system/$MODEL_SERVICE&> /dev/null 
@@ -82,9 +105,21 @@ if [ "$quiet" -eq "0" ];
     systemctl reset-failed
 
     # DELETE PROJECT
+    echo
+    echo "Deleting permanently Project..."
+    echo "    Project path: $MODEL_PATH"
     rm -r $MODEL_PATH
 else
+    # DELETE PCKGS
+    echo
+    echo "The packages from the following environment will be REMOVED:"
+    echo "    Package Plan: $MODEL_ENV"
+    $CONDA_PATH remove --prefix $MODEL_ENV --all --force -y&> /dev/null
+
     # DELETE SERVICE
+    echo
+    echo "Deleting Model Service..."
+    echo "    Service path: /lib/systemd/system/$MODEL_SERVICE"
     rm -rf /lib/systemd/system/$MODEL_SERVICE&> /dev/null
     rm -rf /etc/systemd/system/$MODEL_SERVICE&> /dev/null
     rm -rf /etc/systemd/system/$MODEL_SERVICE&> /dev/null # and symlinks that might be related
@@ -94,6 +129,9 @@ else
     systemctl reset-failed&> /dev/null
 
     # DELETE PROJECT
+    echo
+    echo "Deleting recursively Project..."
+    echo "    Project path: $MODEL_PATH"
     rm -rf $MODEL_PATH
 fi
 
