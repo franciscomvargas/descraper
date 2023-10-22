7916cc310af85785133c5a59a0342b2e5ff4445f
Francisco Vargas <87917356+franciscomvargas@users.noreply.github.com>
2023-10-22 03:46:14 +0100

    Model Standardize for DeSOTA DEV

--- executables/Linux/create_service.py
+++ executables/Linux/create_service.py
@@ -1,14 +1,14 @@
 import os
-CURR_PATH = os.path.dirname(os.path.realpath(__file__))
-
-TARGET_RUN_FILE = os.path.join(CURR_PATH, "descraper.service.bash")
-TARGET_SERV_FILE = os.path.join(CURR_PATH, "descraper.service")
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-uh", "--user_home", 
     help="Specify User PATH to handle with admin requests",
     type=str)
 args = parser.parse_args()
+
+
+
+
 USER_PATH=None
 if args.user_home:
     if os.path.isdir(args.user_home):
@@ -16,22 +16,41 @@ if args.user_home:
 if not USER_PATH:
     USER_PATH = os.path.expanduser('~')
 
+
+
+# -- Edit bellow vvvv DeSOTA DEVELOPER EXAMPLe: miniconda + pip pckgs + systemctl service
+
+CURR_PATH = os.path.dirname(os.path.realpath(__file__))
+
+TARGET_RUN_FILE = os.path.join(CURR_PATH, "descraper.service.bash")
+TARGET_SERV_FILE = os.path.join(CURR_PATH, "descraper.service")
+
+MODEL_PATH=os.path.join(USER_PATH, "Desota", "Desota_Models", "DeScraper")
+MODEL_ENV=os.path.join(MODEL_PATH, "env")
+SERV_DESC="Desota/DeScraper - WebScraper assisted by AI Models "
+SERV_PORT=8880
+SERV_RUN_CMD=f"/bin/bash {TARGET_RUN_FILE}"
+
+
+
+# -- Edit bellow if you're felling lucky ;) -- https://youtu.be/5NV6Rdv1a3I
+
 # SERVICE RUNNER
-TEMPLATE_SERVICE_RUNNER='''#!/bin/bash
+TEMPLATE_SERVICE_RUNNER=f'''#!/bin/bash
 # GET USER PATH
 while true
 do
-    %USER_HOME%/Desota/Desota_Models/DeScraper/env/bin/python3 %USER_HOME%/Desota/Desota_Models/DeScraper/cli.py ui --host 127.0.0.1 --port 8880
+    {MODEL_ENV}/bin/python3 {MODEL_PATH}/cli.py ui --host 127.0.0.1 --port {SERV_PORT}
 done
 # Inform Crawl Finish
 echo Service as Terminated !'''
 
 with open(TARGET_RUN_FILE, "w") as fw:
-    fw.write(TEMPLATE_SERVICE_RUNNER.replace("%USER_HOME%", USER_PATH))
+    fw.write(TEMPLATE_SERVICE_RUNNER)
 
 # SERVICE FILE
-TEMPLATE_SERVICE='''[Unit]
-Description=Desota/DeScraper - WebScraper assisted by AI Models 
+TEMPLATE_SERVICE=f'''[Unit]
+Description={SERV_DESC}
 After=network.target
 StartLimitIntervalSec=0
 StartLimitBurst=5
@@ -41,17 +60,16 @@ StartLimitAction=reboot.
 Type=simple
 Restart=always
 RestartSec=2
-ExecStart=/bin/bash %USER_HOME%/Desota/Desota_Models/DeScraper/executables/Linux/descraper.service.bash
+ExecStart={SERV_RUN_CMD}
 
 [Install]
 WantedBy=multi-user.target'''
 
 with open(TARGET_SERV_FILE, "w") as fw:
-    fw.write(TEMPLATE_SERVICE.replace("%USER_HOME%", USER_PATH))
+    fw.write(TEMPLATE_SERVICE)
     
     
 USER=USER_PATH.split("/")[-1]
 os.system(f"chown -R {USER} {TARGET_RUN_FILE}")
 os.system(f"chown -R {USER} {TARGET_SERV_FILE}")
 exit(0)
-
